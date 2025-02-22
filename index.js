1103a7d534772d853d62cff5f1b94dbef53f19b6
mahbubHossen-dev <mhbabu2002@gmail.com>
2025-02-22 15:57:32 +0600

    create user post api and task post api

--- index.js
+++ index.js
@@ -27,6 +27,27 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+        const db= client.db('ph-task')
+        const userCollection = db.collection('users')
+        const taskCollection = db.collection('taskData')
+        app.post('/users/:email', async(req, res) => {
+            const user = req.body
+            const email = req.params.email;
+            const query = {email}
+            const isExist = await userCollection.findOne(query)
+            if(isExist){
+                return res.send(isExist)
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })  
+
+        app.post('/tasks', async(req, res) => {
+            const data = req.body
+            const result = await taskCollection.insertOne(data)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
